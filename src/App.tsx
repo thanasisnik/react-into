e9af38a329f593b6974d1fa90014c52412d5e82f
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2025-05-27 16:44:12 +0300

    feat: components with useState, and custom Hooks added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,56 @@
 // import ViteIntro from "./pages/ViteIntro.tsx";
 
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+import Layout from "./components/Layout.tsx";
+// import UserCardComponentWithProps from "./components/exercise/UserCardComponentWithProps.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
+import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
+import CounterAdvanced from "./components/CounterAdvanced.tsx";
+import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
+import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+// import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+// import Counter from "./components/Counter.tsx";
+// import logo from '/logo.png'
 
 function App() {
 
 
-  return (
+  // @ts-ignore
+    // @ts-ignore
+    // @ts-ignore
+    return (
     <>
-      {/*<ViteIntro/>*/}
-        <ClassComponent />
-        <FunctionalComponent />
-        <ArrowFunctionalComponent />
-        <ArrowFunctionalComponentWithPropsType title={"Hello World"}
-        description={"Welcome to React into!"}/>
+        <Layout>
+
+                {/*<ClassComponent />*/}
+                {/*<FunctionalComponent />*/}
+                {/*<ArrowFunctionalComponent />*/}
+                {/*<ArrowFunctionalComponentWithPropsType title={"Hello World"}*/}
+                {/*description={"Welcome to React into!"}/>*/}
+
+            {/*<h1 className="text-center text-2xl font-bold">Hello</h1>*/}
+            {/*<FunctionalComponent />*/}
+
+            {/*<ClassComponentWithState></ClassComponentWithState>*/}
+            {/*<FunctionalComponentWithState/>*/}
+            {/*<Counter />*/}
+            {/*<UserCardComponentWithProps name={"Athanasios"} surname={"Nikolopoulos"} age={27} title={"Software Engineer"} description={"Junior Full Stack Developer"} photo={logo} />*/}
+            {/*<NameChanger />*/}
+            <CounterWithMoreStates />
+            <CounterAdvanced />
+            <CounterWithCustomHook/>
+            <CounterAdvancedWithCustomHook/>
+
+
+        </Layout>
+
     </>
   )
 }
---
+++ src/components/CounterWithMoreStates.tsx
@@ -0,0 +1,50 @@
+import {useState} from "react";
+import CounterButton from "./CounterButton.tsx";
+
+
+const CounterWithMoreStates = () => {
+    const [count, setCount] = useState(0);
+    const [lastAction, setLastAction] = useState("");
+    const [time, setTime] = useState("");
+
+    const getCurrentTime = () => new Date().toLocaleTimeString();
+
+    const increaseCount = () => {
+        setCount(count + 1);
+        setLastAction("Increase");
+        setTime(getCurrentTime());
+    }
+
+    const resetCount = () => {
+        setCount(0);
+        setLastAction("Reset");
+        setTime(getCurrentTime());
+    }
+
+    const decreaseCount = () => {
+        if (count > 0) {
+            setCount(count - 1);
+            setLastAction("Decrease")
+            setTime(getCurrentTime());
+        }
+    }
+
+    return (
+        <>
+            <div className="space-y-4 pt-12">
+                <h1 className="text-center">Count is {count}</h1>
+                <div className="text-center space-x-4">
+
+                    <CounterButton onClick={increaseCount}  label={"Increase"}/>
+                    <CounterButton onClick={decreaseCount} disabled={count === 0} label={"Decrease"}/>
+                    <CounterButton onClick={resetCount} disabled={count === 0} label={"Reset"} addClass="bg-red-400"/>
+
+
+                </div>
+            </div>
+            <p>Last change: <strong>{lastAction}</strong> at <strong>{time}</strong></p>
+        </>
+    )
+}
+
+export default CounterWithMoreStates;
